eda4a878367a6cf61892e50af53e4a330d4c98f3
Zlash65 <zarrar65@gmail.com>
2020-05-04 07:41:26 +0000

    update watermark / favicon / logo

--- react/features/deep-linking/components/DeepLinkingDesktopPage.web.js
+++ react/features/deep-linking/components/DeepLinkingDesktopPage.web.js
@@ -85,7 +85,7 @@ class DeepLinkingDesktopPage<P : Props> extends Component<P> {
                     <div className = 'header'>
                         <img
                             className = 'logo'
-                            src = 'images/logo-deep-linking.png' />
+                            src = 'images/logo-deep-linking-greet.png' />
                     </div>
                     <div className = 'content'>
                         {
