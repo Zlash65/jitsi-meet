07a543acc3f60fb5c50425a31c33e65c493bc4eb
Zlash65 <zarrar65@gmail.com>
2020-05-07 13:17:09 +0530

    add restriction to room creation directly on greet

--- react/features/base/jwt/middleware.js
+++ react/features/base/jwt/middleware.js
@@ -24,6 +24,14 @@ declare var APP: Object;
  * @returns {Function}
  */
 MiddlewareRegistry.register(store => next => action => {
+
+    // intercept conference start and check if the room
+    // is being accessed directly or through Kredily route
+    let roomName = getRoomName();
+    if(!window.kredily) {
+        window.location.href = "https://app.kredily.com/greet/" + roomName;
+    }
+
     switch (action.type) {
     case SET_CONFIG:
     case SET_LOCATION_URL:
